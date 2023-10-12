4197e5637be9c242e67cec2ec1d2290adafce40f
solidsnakedev <jona.ca.eng@gmail.com>
2023-10-12 13:16:27 -0400

    improve error handling

--- run-order-bot.sh
+++ run-order-bot.sh
@@ -1,3 +1,10 @@
+printf "Running geniusyield order bot: \n"
+printf "PAYMENT_SIGNING_KEY_CBOR_HEX=${PAYMENT_SIGNING_KEY_CBOR_HEX:?"missing variable"}\n"
+printf "COLLATERAL_UTXO_REF=${COLLATERAL_UTXO_REF:?"missing variable"}\n"
+printf "BLOCKFROST_API_KEY=${BLOCKFROST_API_KEY:?"missing variable"}\n"
+printf "CARDANO_NETWORK=${CARDANO_NETWORK:?"missing variable"}\n"
+
+
 docker run -i \
     -e BOTC_SKEY="{\"cborHex\": \"$PAYMENT_SIGNING_KEY_CBOR_HEX\", \"type\": \"PaymentSigningKeyShelley_ed25519\", \"description\": \"Payment Signing Key\"}" \
     -e BOTC_COLLATERAL="$COLLATERAL_UTXO_REF" \
