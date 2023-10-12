151d0fee563c4ad7b1664fda41ddca6fdfa12f7e
solidsnakedev <jona.ca.eng@gmail.com>
2023-10-12 19:56:48 -0400

    update script

--- README.md
+++ README.md
@@ -18,11 +18,18 @@ chmod +x cardano-cli
 ./cardano-cli address key-gen \
 	--verification-key-file bot.vkey \
 	--signing-key-file bot.skey
+```
 
+```
 ./cardano-cli address build \
     --payment-verification-key-file bot.vkey \
     --testnet-magic 1 \
     --out-file bot.preprod.addr
+```
+
+## Get signing key
+```
+cat bot.skey | jq .cborHex
 ```
 
 # SMART ORDER ROUTER INSTANCE USING BLOCKFROST
@@ -31,9 +38,12 @@ chmod +x cardano-cli
 
 ```
 export PAYMENT_SIGNING_KEY_CBOR_HEX=5820d682e237a04d43ad011fdecd141acd485f6d3d634466692d58f6d75250f39134
-export COLLATERAL_UTXO_REF=7cc7b044d26981d3fc73ae72994f289d99ba113ceefb5b83f4d7643bfb12682a#1
 export BLOCKFROST_API_KEY=some_api_key
 export CARDANO_NETWORK=testnet-preprod
+```
+## Optional
+```
+export COLLATERAL_UTXO_REF=7cc7b044d26981d3fc73ae72994f289d99ba113ceefb5b83f4d7643bfb12682a#1
 ```
 
 ## Run order bot
--- run-order-bot.sh
+++ run-order-bot.sh
@@ -1,12 +1,20 @@
 printf "Running geniusyield order bot: \n"
 printf "PAYMENT_SIGNING_KEY_CBOR_HEX=${PAYMENT_SIGNING_KEY_CBOR_HEX:?"missing variable"}\n"
-printf "COLLATERAL_UTXO_REF=${COLLATERAL_UTXO_REF:?"missing variable"}\n"
+# printf "COLLATERAL_UTXO_REF=${COLLATERAL_UTXO_REF:?"missing variable"}\n"
 printf "BLOCKFROST_API_KEY=${BLOCKFROST_API_KEY:?"missing variable"}\n"
 printf "CARDANO_NETWORK=${CARDANO_NETWORK:?"missing variable"}\n"
 
-
-docker run -i \
-    -e BOTC_SKEY="{\"cborHex\": \"$PAYMENT_SIGNING_KEY_CBOR_HEX\", \"type\": \"PaymentSigningKeyShelley_ed25519\", \"description\": \"Payment Signing Key\"}" \
-    -e BOTC_COLLATERAL="$COLLATERAL_UTXO_REF" \
-    -e BOTC_CONFIG="{ \"coreProvider\": { \"blockfrostKey\": \"$BLOCKFROST_API_KEY\" }, \"networkId\": \"$CARDANO_NETWORK\", \"logging\": [{ \"type\": { \"tag\": \"stderr\" }, \"severity\": \"Info\", \"verbosity\": \"V2\" }],\"utxoCacheEnable\": false }" \
-    ghcr.io/geniusyield/smart-order-router:latest
+if [ -z ${COLLATERAL_UTXO_REF} ]; then
+    printf "\nrunning without COLLATERAL_UTXO_REF \n\n"
+    docker run -i \
+        -e BOTC_SKEY="{\"cborHex\": \"$PAYMENT_SIGNING_KEY_CBOR_HEX\", \"type\": \"PaymentSigningKeyShelley_ed25519\", \"description\": \"Payment Signing Key\"}" \
+        -e BOTC_CONFIG="{ \"coreProvider\": { \"blockfrostKey\": \"$BLOCKFROST_API_KEY\" }, \"networkId\": \"$CARDANO_NETWORK\", \"logging\": [{ \"type\": { \"tag\": \"stderr\" }, \"severity\": \"Info\", \"verbosity\": \"V2\" }],\"utxoCacheEnable\": false }" \
+        ghcr.io/geniusyield/smart-order-router:latest
+else
+    printf "\nrunning with COLLATERAL_UTXO_REF \n\n"
+    docker run -i \
+        -e BOTC_SKEY="{\"cborHex\": \"$PAYMENT_SIGNING_KEY_CBOR_HEX\", \"type\": \"PaymentSigningKeyShelley_ed25519\", \"description\": \"Payment Signing Key\"}" \
+        -e BOTC_COLLATERAL="$COLLATERAL_UTXO_REF" \
+        -e BOTC_CONFIG="{ \"coreProvider\": { \"blockfrostKey\": \"$BLOCKFROST_API_KEY\" }, \"networkId\": \"$CARDANO_NETWORK\", \"logging\": [{ \"type\": { \"tag\": \"stderr\" }, \"severity\": \"Info\", \"verbosity\": \"V2\" }],\"utxoCacheEnable\": false }" \
+        ghcr.io/geniusyield/smart-order-router:latest
+fi
