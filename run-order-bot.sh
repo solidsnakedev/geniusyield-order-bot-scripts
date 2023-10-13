7745137b67ef88aac8665d36f582e657311c05d8
solidsnakedev <jona.ca.eng@gmail.com>
2023-10-12 21:00:33 -0400

    fix syntax error

--- run-order-bot.sh
+++ run-order-bot.sh
@@ -7,17 +7,17 @@ printf "CARDANO_NETWORK=${CARDANO_NETWORK:?"missing variable"}\n"
 if [ -z ${COLLATERAL_UTXO_REF} ]; then
     printf "\nrunning without COLLATERAL_UTXO_REF \n\n"
     docker run -d \
-        --name geniusyield-oder-bot
+        --name geniusyield-oder-bot \
         -e BOTC_SKEY="{\"cborHex\": \"$PAYMENT_SIGNING_KEY_CBOR_HEX\", \"type\": \"PaymentSigningKeyShelley_ed25519\", \"description\": \"Payment Signing Key\"}" \
         -e BOTC_CONFIG="{ \"coreProvider\": { \"blockfrostKey\": \"$BLOCKFROST_API_KEY\" }, \"networkId\": \"$CARDANO_NETWORK\", \"logging\": [{ \"type\": { \"tag\": \"stderr\" }, \"severity\": \"Info\", \"verbosity\": \"V2\" }],\"utxoCacheEnable\": false }" \
-        ghcr.io/geniusyield/smart-order-router:latest \
+        ghcr.io/geniusyield/smart-order-router:latest
 
 else
     printf "\nrunning with COLLATERAL_UTXO_REF \n\n"
     docker run -d \
-        --name geniusyield-oder-bot
+        --name geniusyield-oder-bot \
         -e BOTC_SKEY="{\"cborHex\": \"$PAYMENT_SIGNING_KEY_CBOR_HEX\", \"type\": \"PaymentSigningKeyShelley_ed25519\", \"description\": \"Payment Signing Key\"}" \
         -e BOTC_COLLATERAL="$COLLATERAL_UTXO_REF" \
         -e BOTC_CONFIG="{ \"coreProvider\": { \"blockfrostKey\": \"$BLOCKFROST_API_KEY\" }, \"networkId\": \"$CARDANO_NETWORK\", \"logging\": [{ \"type\": { \"tag\": \"stderr\" }, \"severity\": \"Info\", \"verbosity\": \"V2\" }],\"utxoCacheEnable\": false }" \
-        ghcr.io/geniusyield/smart-order-router:latest \
+        ghcr.io/geniusyield/smart-order-router:latest
 fi
