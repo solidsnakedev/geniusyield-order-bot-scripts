b3fbe879f4668c896fe261d33e122a30f6f1c5a6
solidsnakedev <jona.ca.eng@gmail.com>
2023-10-15 09:12:09 -0400

    update script

--- run-order-bot.sh
+++ run-order-bot.sh
@@ -3,20 +3,16 @@ source .env.local
 printf "Loading .env.local\n\n"
 cat .env.local
 
-printf "PAYMENT_SIGNING_KEY_CBOR_HEX=${PAYMENT_SIGNING_KEY_CBOR_HEX:?"missing variable"}\n"
-printf "CARDANO_NETWORK=${CARDANO_NETWORK:?"missing variable"}\n"
-
-printf "Running geniusyield order bot: \n"
+printf "\nRunning geniusyield order bot:\n"
 if [ -z ${COLLATERAL_UTXO_REF} ]; then
-    if [ -n ${BLOCKFROST_API_KEY} ]; then
+    if [ ! -z ${BLOCKFROST_API_KEY} ]; then
         printf "BLOCKFROST_API_KEY is set, using blockfrost\n"
-        printf "${BLOCKFROST_API_KEY}"
         docker run -d \
             --name geniusyield-order-bot \
             -e BOTC_SKEY="{\"cborHex\": \"$PAYMENT_SIGNING_KEY_CBOR_HEX\", \"type\": \"PaymentSigningKeyShelley_ed25519\", \"description\": \"Payment Signing Key\"}" \
             -e BOTC_CONFIG="{ \"coreProvider\": { \"blockfrostKey\": \"$BLOCKFROST_API_KEY\" }, \"networkId\": \"$CARDANO_NETWORK\", \"logging\": [{ \"type\": { \"tag\": \"stderr\" }, \"severity\": \"Info\", \"verbosity\": \"V2\" }],\"utxoCacheEnable\": false }" \
             ghcr.io/geniusyield/smart-order-router:latest
-    elif [ -n ${MAESTRO_API_KEY} ]; then
+    elif [ ! -z ${MAESTRO_API_KEY} ]; then
         printf "MAESTRO_API_KEY is set, using maestro\n"
         docker run -d \
             --name geniusyield-order-bot \
@@ -29,7 +25,7 @@ if [ -z ${COLLATERAL_UTXO_REF} ]; then
     fi
 
 else
-    if [ -n ${BLOCKFROST_API_KEY} ]; then
+    if [ ! -z ${BLOCKFROST_API_KEY} ]; then
         printf "BLOCKFROST_API_KEY is set, using blockfrost\n"
         docker run -d \
             --name geniusyield-order-bot \
@@ -37,7 +33,7 @@ else
             -e BOTC_COLLATERAL="$COLLATERAL_UTXO_REF" \
             -e BOTC_CONFIG="{ \"coreProvider\": { \"blockfrostKey\": \"$BLOCKFROST_API_KEY\" }, \"networkId\": \"$CARDANO_NETWORK\", \"logging\": [{ \"type\": { \"tag\": \"stderr\" }, \"severity\": \"Info\", \"verbosity\": \"V2\" }],\"utxoCacheEnable\": false }" \
             ghcr.io/geniusyield/smart-order-router:latest
-    elif [ -n ${MAESTRO_API_KEY} ]; then
+    elif [ ! -z ${MAESTRO_API_KEY} ]; then
         printf "MAESTRO_API_KEY is set, using maestro\n"
         docker run -d \
             --name geniusyield-order-bot \
