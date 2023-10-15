6bf628e50a39fb5d00a95714af079070cc4e998e
solidsnakedev <jona.ca.eng@gmail.com>
2023-10-15 08:41:08 -0400

    update

--- run-order-bot.sh
+++ run-order-bot.sh
@@ -14,7 +14,7 @@ if [ -z ${COLLATERAL_UTXO_REF} ]; then
             -e BOTC_CONFIG="{ \"coreProvider\": { \"blockfrostKey\": \"$BLOCKFROST_API_KEY\" }, \"networkId\": \"$CARDANO_NETWORK\", \"logging\": [{ \"type\": { \"tag\": \"stderr\" }, \"severity\": \"Info\", \"verbosity\": \"V2\" }],\"utxoCacheEnable\": false }" \
             ghcr.io/geniusyield/smart-order-router:latest
     elif [ -n ${MAESTRO_API_KEY} ]; then
-        docker run -it \
+        docker run -d \
             -e BOTC_SKEY="{\"cborHex\": \"$PAYMENT_SIGNING_KEY_CBOR_HEX\", \"type\": \"PaymentSigningKeyShelley_ed25519\", \"description\": \"Payment Signing Key\"}" \
             -e BOTC_CONFIG="{ \"coreProvider\": { \"maestroToken\": \"$MAESTRO_API_KEY\" }, \"networkId\": \"$CARDANO_NETWORK\", \"logging\": [{ \"type\": { \"tag\": \"stderr\" }, \"severity\": \"Info\", \"verbosity\": \"V2\" }],\"utxoCacheEnable\": false }" \
             ghcr.io/geniusyield/smart-order-router:latest
@@ -32,7 +32,7 @@ else
             -e BOTC_CONFIG="{ \"coreProvider\": { \"blockfrostKey\": \"$BLOCKFROST_API_KEY\" }, \"networkId\": \"$CARDANO_NETWORK\", \"logging\": [{ \"type\": { \"tag\": \"stderr\" }, \"severity\": \"Info\", \"verbosity\": \"V2\" }],\"utxoCacheEnable\": false }" \
             ghcr.io/geniusyield/smart-order-router:latest
     elif [ -n ${MAESTRO_API_KEY} ]; then
-        docker run -it \
+        docker run -d \
             -e BOTC_SKEY="{\"cborHex\": \"$PAYMENT_SIGNING_KEY_CBOR_HEX\", \"type\": \"PaymentSigningKeyShelley_ed25519\", \"description\": \"Payment Signing Key\"}" \
             -e BOTC_COLLATERAL="$COLLATERAL_UTXO_REF" \
             -e BOTC_CONFIG="{ \"coreProvider\": { \"maestroToken\": \"$MAESTRO_API_KEY\" }, \"networkId\": \"$CARDANO_NETWORK\", \"logging\": [{ \"type\": { \"tag\": \"stderr\" }, \"severity\": \"Info\", \"verbosity\": \"V2\" }],\"utxoCacheEnable\": false }" \
